178b6e566c9daa829cd803cb42efcad2c81e2dc8
Nayheli Cahuaza <nayheli.alexandra09@gmail.com>
2019-10-29 07:30:17 -0500

    input

---
+++ Trabajo.py
@@ -0,0 +1,8 @@
+#input
+
+grado = int(input("Ingresa tu grado que estas cursando: "))
+colegio = input("Ingresa el nombre de tu colegio: ")
+nombre = input("Escriba su nombre: ")
+apellido = input("Escriba su apellido ")
+dni =  int(input("Escriba el numero de su DNI: "))
+cursos = input("Escriba sus cursos preferidos")
