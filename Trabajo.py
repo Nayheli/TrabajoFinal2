71bab616c51a8aa8e98219ec6bdc2974ab53ac45
alumno informatica <alumno@ciis.usil.edu.pe>
2019-10-29 07:58:59 -0500

    ActualizadoProceso

--- Trabajo.py
+++ Trabajo.py
@@ -1,8 +1,60 @@
 #Process
+import os
+os.system("cls")
 
+print("------------Bienvenido al Aula Virtual------------")
 grado = int(input("Ingresa el grado que estas cursando: "))
+while True:
+    if grado <= 0:
+        print("Error, ingrese dato correcto")
+        grado = int(input("Ingresa el grado que estas cursando: "))
+    else:
+        break
+
+edad = int(input("Ingresa tu edad: "))
+while True:
+    if edad <= 0:
+        print("Error, ingrese dato correcto")
+        colegio = input("Ingresa el nombre de tu colegio: ")
+    else:
+        break
+
 colegio = input("Ingresa el nombre de tu colegio: ")
+while True:
+    if colegio == "":
+        print("Error, ingrese dato correcto")
+        colegio = input("Ingresa el nombre de tu colegio: ")
+    else:
+        break
+
 nombre = input("Escriba su nombre: ")
+while True:
+    if nombre == "":
+        print("Error, ingrese dato correcto")
+        nombre = input("Escriba su nombre: ")
+    else:
+        break
+
 apellido = input("Escriba su apellido: ")
+while True:
+    if apellido == "":
+        print("Error, ingrese dato correcto")
+        apellido = input("Escriba su apellido: ")
+    else:
+        break
+
 dni =  int(input("Escriba el numero de su DNI: "))
+while True:
+    if dni <= 0:
+        print("Error, ingrese dato correcto")
+        dni =  int(input("Escriba el numero de su DNI: "))
+    else:
+        break
+
 cursos = input("Escriba los cursos que va a llevar:")
+while True:
+    if cursos == "":
+        print("Error, ingrese dato correcto")
+        cursos = input("Escriba los cursos que va a llevar:")
+    else:
+        break
