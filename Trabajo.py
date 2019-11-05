b85eed931491797bee38507939de0ad045f98804
Nayheli Cahuaza <nayheli.alexandra09@gmail.com>
2019-11-05 08:43:45 -0500

    okis

--- Trabajo.py
+++ Trabajo.py
@@ -96,10 +96,26 @@ print("Cursos escogidos:", lista_cursos)
 
 
 print("-------------Horarios--------------")
-HorarioInicialMate = 9
-HorarioFinalMate = 4
-HorarioInicialLogi = 8
-HorarioFinalLogi = 3
+#horario de primaria
+Horario1 = "7:00 am"
+Horario2 = "9:00 am"
+Horario3 = "11:00 am"
+Recreo1 = "12:00 am - 12:30 pm"
+Horario5 = "1:00 pm"
+Horario6 = "2:00 pm"
+#horario de secundaria
+Horario7 = "7:00 am"
+Horario8 = "9:00 am"
+Horario9 = "11:00 am"
+Recreo2 = "12:00 pm - 12:30 pm"
+Horario11 = "1:00 pm"
+Horario12 = "2:00 pm"
+Horario13 = "3:00 pm" 
+
+
+
+if nivel == "primaria":
+    print(lista_cursos[2],"este curso lo llevaras a las",Horario1)
 
 
 
